87d4a8e09d55d888631b2a516304d1bde77db005
KadirAKIN <58920213+KadirAKIN@users.noreply.github.com>
2019-12-16 02:40:23 +0300

    Update global.js

--- global.js
+++ global.js
@@ -1,23 +1,5 @@
 (function() {
 
-  var allNavItems = $(".main-nav li");
-  var allTabs = $(".tab");
-
-  allNavItems.on("click", function(e) {
-
-    e.preventDefault();
-
-    allNavItems.removeClass("active");
-    allTabs.removeClass("active");
-
-    // this = list item
-    $(this).addClass("active");
-
-    var selector = $(this).find("a").attr("href");
-    $(selector).addClass("active");
-
-  });
-
   // ServiceWorker is a progressive technology. Ignore unsupported browsers
   if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
