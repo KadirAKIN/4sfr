18d88466da8befb45e4663fb6714e33e8f336d5e
KadirAKIN <58920213+KadirAKIN@users.noreply.github.com>
2019-12-16 03:46:58 +0300

    Update global.js

--- global.js
+++ global.js
@@ -1,12 +1,13 @@
 (function() {
   
-  console.log(window.location.search.substr(1));
+  
   
   // ServiceWorker is a progressive technology. Ignore unsupported browsers
   if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
     navigator.serviceWorker.register('sw.js').then(function() {
       console.log('CLIENT: service worker registration complete.');
+      console.log(window.location.search.substr(1));
     }, function() {
       console.log('CLIENT: service worker registration failure.');
     });
